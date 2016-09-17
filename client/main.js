2a96512e612858827575c0b45b1cb5839b21e20f
Brian Morris <bkmorris8@gmail.com>
2016-09-17 17:26:19 -0400

    added graph, pulling data using api request

--- client/main.js
+++ client/main.js
@@ -5,41 +5,56 @@ import './main.html';
 
 Template.chartarea.topGenresChart = function() {
     return {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
         title: {
-            text: this.username + "'s top genres"
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
         },
         tooltip: {
-            pointFormat: '<b>{point.percentage:.1f}%</b>'
+            valueSuffix: '°C'
         },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    },
-                    connectorColor: 'silver'
-                }
-            }
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
         },
         series: [{
-            type: 'pie',
-            name: 'genre',
-            data: [
-                ['Adventure',   45.0],
-                ['Action',       26.8],
-                ['Ecchi',   12.8],
-                ['Comedy',    8.5],
-                ['Yuri',     6.2]
-            ]
+            name: 'Bitcoin',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     };
 };
+
+//invoke the server method
+if (Meteor.isClient) {
+    Meteor.call("pullCoinData", function(error, results) {
+        console.log(results.content); //results.data should be a JSON object
+    });
+};   
--- server/main.js
+++ server/main.js
@@ -1,5 +1,33 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// HTTP.call( 'GET', 'https://api.coinmarketcap.com/v1/ticker/bitcoin/', {}, function( error, response ) {
+	//   if ( error ) {
+	//     console.log( error );
+	//   } else {
+	//     console.log( response );
+	    
+	//      This will return the HTTP response object that looks something like this:
+	//      {
+	//        content: "String of content...",
+	//        data: Array[100], <-- Our actual data lives here. 
+	//        headers: {  Object containing HTTP response headers }
+	//        statusCode: 200
+	//      }
+	    
+	//   }
+	// });
+
+	if (Meteor.isServer) {
+	    Meteor.methods({
+	        pullCoinData: function () {
+	            this.unblock();
+	            return Meteor.http.call("GET", "https://www.quandl.com/api/v1/datasets/BAVERAGE/USD.json");
+	            // return Meteor.http.call("GET", "https://api.coinmarketcap.com/v1/ticker/bitcoin/");
+	        }
+	    });
+	}
+
+
 });
