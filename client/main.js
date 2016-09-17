cbfd3256dd90957f453699128bc73759431fa46d
Brian Morris <bkmorris8@gmail.com>
2016-09-17 16:34:55 -0400

    taking shape

--- client/main.js
+++ client/main.js
@@ -13,10 +13,3 @@ Template.hello.helpers({
     return Template.instance().counter.get();
   },
 });
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
